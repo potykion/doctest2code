68b9b481d15765442b194de4206010061a87d62d
potykion <potykion@gmail.com>
2018-07-07 14:24:07 +0300

    - Dots as empty doctest line - Fix asserts append for non-doctest line

--- doctest2code/utils.py
+++ doctest2code/utils.py
@@ -1,15 +1,48 @@
+import re
+from typing import Iterable
+
+
 def doctest_to_code(doctest: str) -> str:
-    code_lines = []
-
-    lines = doctest.splitlines()
-    for line in lines:
-        if line.startswith('>>> ') or line.startswith('... '):
-            line = line[4:]
-            code_lines.append(line)
-        elif code_lines:
-            code_lines[-1] = f'assert {code_lines[-1]} == {line}'
-        else:
-            code_lines.append(line)
-
+    doctest_lines = doctest.splitlines()
+    code_lines = _to_code_lines(doctest_lines)
     code = '\n'.join(code_lines)
     return code
+
+
+def _to_code_lines(lines: Iterable[str]) -> Iterable[str]:
+    code_lines = []
+    previous_line_is_doctest = False
+
+    for line in lines:
+        if line.startswith('>>>') or line.startswith('...'):
+            code_lines.append(_strip_doctest_line(line))
+            previous_line_is_doctest = True
+        elif previous_line_is_doctest:
+            code_lines[-1] = f'assert {code_lines[-1]} == {line}'
+            previous_line_is_doctest = False
+        else:
+            code_lines.append(line)
+            previous_line_is_doctest = False
+
+    return code_lines
+
+
+def _strip_doctest_line(line: str) -> str:
+    """
+    >>> _strip_doctest_line(">>> def op(a)")
+    'def op(a)'
+    >>> _strip_doctest_line("... return a")
+    'return a'
+    >>> _strip_doctest_line("... ")
+    ''
+    >>> _strip_doctest_line("...    ")
+    ''
+    >>> _strip_doctest_line("...")
+    ''
+    """
+    stripped = re.sub("(>>>|\.\.\.)\s?", "", line)
+
+    if re.match("\s*$", stripped):
+        stripped = ""
+
+    return stripped
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,22 +3,20 @@ import pytest
 from doctest2code.utils import doctest_to_code
 
 
-@pytest.fixture()
-def doctest():
-    return """>>> sum_ = 0
->>> for num in range(5):
-...   sum_ += num
->>> sum_
-10"""
+@pytest.mark.parametrize("doctest, code", [
+    (
+            ">>> sum_ = 0\n>>> for num in range(5):\n...   sum_ += num\n>>> sum_\n10",
+            "sum_ = 0\nfor num in range(5):\n  sum_ += num\nassert sum_ == 10"
+    ),
+    (
+            "as\nas\nas",
+            "as\nas\nas"
+    ),
+    (
+            ">>> def op(*, oppa=None):\n...   return oppa\n...\n>>> op(oppa=\"oppa\")\n'oppa'",
+            "def op(*, oppa=None):\n  return oppa\n\nassert op(oppa=\"oppa\") == 'oppa'"
+    )
 
-
-@pytest.fixture()
-def code():
-    return """sum_ = 0
-for num in range(5):
-  sum_ += num
-assert sum_ == 10"""
-
-
+])
 def test_doctest_to_code(doctest, code):
     assert doctest_to_code(doctest) == code
